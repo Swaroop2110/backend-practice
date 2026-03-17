1d045139a91510e0def16b49df93c273036ab7d4
Swaroop Gupta <159252915+Swaroop2110@users.noreply.github.com>
2026-03-17 23:25:14 +0530

    setup

--- src/app.js
+++ src/app.js
@@ -12,4 +12,16 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+// routes import
+
+import userRouter from './routes/user.routes.js'
+
+// routes declartion
+
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:800/users/register
+
+
 export {app}
